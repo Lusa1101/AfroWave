6fce676d751ea62aceab33480f2ba6d7190937e8
Lusa1101 <omphu.shau@outlook.com>
2025-09-14 19:01:27 +0200

    fix: model.ts

--- AfroWave/src/services/model.ts
+++ AfroWave/src/services/model.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ModelService {
-  baseUrl = 'http://localhost:5030/Model/PredictAllModels';
+  baseUrl = 'http://localhost:5030/Model/PredictTopGenre';
   baseUrl2 = 'http://localhost:5030/Model/PredictTop3Genres';
   baseUrl3 = 'https://samgc.runasp.net//PredictAllModels';
 
@@ -28,6 +28,6 @@ export class ModelService {
   async getAllPredictions(audio_path: string): Promise<Observable<any>> {
     const params = { audio_path: audio_path };
 
-    return await this.http.get<any>(this.baseUrl, {params});
+    return await this.http.get<any>(this.baseUrl3, {params});
   }
 }
